f1c0301709b5eb0bc86f6277db0907fe48c63d12
Alessio <102510696+AlessioMeraldi@users.noreply.github.com>
2026-01-21 15:20:43 +0100

    View personaggi V1
    - Fatta la View dei personaggi a livelli contenuistici (di base).
    - Fatto il relativo modulo CSS
    - Ancora da fare i commenti decenti e c'è da capire un problema di performance sull'hover dei personaggi.

--- src/simpleViews (components)/Characters.jsx
+++ src/simpleViews (components)/Characters.jsx
@@ -1,15 +1,14 @@
-
 // Style imports
 import styles from "/src/simpleViews (components)/Characters.module.css"
 
 // React imports
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 
 //  ViewModel imports
 import CharactersViewModel from '/src/viewModels/CharactersViewModel.js'
 
 // Begin logic
-function Characters () {
+function Characters() {
 
     // instantiate the ViewModel and get only the parts we're interested in right now
     const {
@@ -23,7 +22,7 @@ function Characters () {
     }, []);
 
     return (
-        <div>
+        <div className={styles.characters}>
 
             {/* toDo: filters section */}
             <section>
@@ -31,13 +30,25 @@ function Characters () {
             </section>
 
             {/* Characters grid */}
-            <main>
-                {allCharacters?.map((character, index) => ( // toDo: clarify the ?
-                    <div key={character.id || index}> {/* attempt to use character's ID and fallback to index if something goes wrong */}
-                        <p> {character?.name} </p> {/* toDo: clarify the ?*/}
-                    </div>
-                ))}
-            </main>
+            <section className={styles.charactersSection}>
+                <h2> Ecco i personaggi </h2>
+
+                {/* toDo: @YuYu - a me sembra che l'hover dei primi personaggi lagghi e non quello degli ultimi, però forse è una mia impressione - mi fai sapere se ti sembra anche a te? */}
+                <div className={styles.grid}>
+                    {allCharacters?.map((character, index) => ( // toDo: clarify the ? with a comment
+                        <div key={character.id || index} className={styles.card}>
+                            <img
+                                src={`https://cdn.thesimpsonsapi.com/200/character/${character.id}.webp`}
+                                alt={character.name}
+                            />
+                            <h3>{character.name}</h3>
+                            <p>{character.occupation}</p>
+                            <p>{character.status}</p>
+
+                        </div>
+                    ))}
+                </div>
+            </section>
 
         </div>
     )
