f35c31cb6dbe9e8bd08487c5439639723b22d13b
yousraelalani <yousraelalani@gmail.com>
2026-02-14 10:10:58 +0100

    Fix Logout button
    Fix Logout button

--- src/simpleViews (components)/ProfileComponents/LogoutButton.jsx
+++ src/simpleViews (components)/ProfileComponents/LogoutButton.jsx
@@ -27,7 +27,6 @@ const LogoutButton = ({ className }) => {
                     strokeWidth="3"
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    className = {styles.svgStyle}
                 >
                     <path d="M18.36 6.64a9 9 0 1 1-12.73 0"></path>
                     <line x1="12" y1="2" x2="12" y2="12"></line>
